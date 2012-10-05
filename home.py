ffa175f675a6c44b15f079d8ee1cadc2fe7d8d71
Andres G. Saravia <ags3006@member.fsf.org>
2012-10-05 16:55:11 -0500

    Added an 'Under construction' page.

--- home.py
+++ home.py
@@ -13,5 +13,12 @@ app = webapp2.WSGIApplication([('/', frontend.MainPage),
                                ('/settings', users.SettingsPage),
                                # Reviews module
                                ('/reviews', reviews.MainPage),
-                               ('/reviews/new', reviews.New)],
+                               ('/reviews/new', reviews.New),
+                               # Empty placeholders
+                               ('/reviews/articles', frontend.UnderConstructionPage),
+                               ('/reviews/blog_posts', frontend.UnderConstructionPage),
+                               ('/news', frontend.UnderConstructionPage),
+                               ('/notebook', frontend.UnderConstructionPage),
+                               ('/classroom', frontend.UnderConstructionPage),
+                               ('/collaborations', frontend.UnderConstructionPage)],
                               debug = True)
--- src/frontend.py
+++ src/frontend.py
@@ -5,3 +5,7 @@ from generic import *
 class MainPage(GenericPage):
     def get(self):
         self.render("base.html")
+
+class UnderConstructionPage(GenericPage):
+    def get(self):
+        self.render("under_construction.html")
