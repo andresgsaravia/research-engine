eb76d6ee0412dff284b334109520ad6ddf55c2a4
Andres G. Saravia <ags3006@member.fsf.org>
2012-10-30 12:29:21 -0600

    Datastore definitions for the Notebooks. #50

--- src/notebooks.py
+++ src/notebooks.py
@@ -13,6 +13,28 @@ from generic import *
 ##   Datastore Objects   ##
 ###########################
 
+# Each Notebook is parent to its NotebookNotes.
+class Notebooks(db.Model):
+    owner = db.ReferenceProperty(required = True)
+    name = db.StringProperty(required = True)
+    description = db.TextProperty(required = True)
+    started = db.DateTimeProperty(auto_now_add = True)
+    last_updated = db.DateTimeProperty(auto_now = True)
+
+
+# Each NotebookNote should have as parent one Notebook. Comments to notes are children.
+class NotebookNotes(db.Model):
+    title = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    added = db.DateTimeProperty(auto_now_add = True)
+
+
+# Each NoteComment should have as parent one NotebookNote.
+class NoteComments(db.Model):
+    content = db.TextProperty(required = True)
+    author = db.ReferenceProperty(required = True)
+    added = db.DateTimeProperty(auto_now_add = True)
+
 
 ######################
 ##   Web Handlers   ##
