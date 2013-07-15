9ac77b3c9a639de76a350fb23f01ef644dd00d8a
Andres G. Saravia <ags3006@member.fsf.org>
2013-07-15 13:17:22 -0500

    doi autolinks now include all non-whitespace characters and correctly encodes the target url (fixes #135).

--- src/filters.py
+++ src/filters.py
@@ -1,17 +1,17 @@
 # Definitions for my own filters
 
-import markdown, re
+import markdown, re, urllib
 
 
 # Parsing doi (digital object identifier
 # I will match a string of the form "doi:this/is.the/actual-doi" (Either doi, DOI or Doi will do)
 # and transform it to [doi:doi:this/is.the/actual-doi](http://dx.doi.org/this/is.the/actual-doi"
 # before passing it to markdown
-DOI_REGEXP = r'(doi|Doi|DOI):[\w\./-]+'
+DOI_REGEXP = r'(doi|Doi|DOI):\S+'
 
 def make_doi_link(doi_match_object):
     s = doi_match_object.group(0)
-    return "[%s](http://dx.doi.org/%s)" % (s, s[4:])
+    return "[%s](http://dx.doi.org/%s)" % (s, urllib.quote(s[4:]))
 
 def md(value):
     value = re.sub(DOI_REGEXP, make_doi_link, value)     # doi links
